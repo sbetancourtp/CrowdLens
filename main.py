af85cd5eebd15a2313d5485815d0421963419da8
sbetancourtp <sbetancourtp@unal.edu.co>
2025-06-04 08:25:00 -0500

    chore: Fix leaked token.
     * Leaked token deactivated and replaced
     * main file modified to hide new token

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from messaging.telegram_client import run_bot
 from data.db.main_db import initialize_db
+from dotenv import load_dotenv
+import os
 
 if __name__ == "__main__":
-    TOKEN = "7781537287:AAHTwrmZEjeY4QU_SlcvDUAc1F72Eqgc5EQ"
+    load_dotenv()
+    TOKEN = os.getenv("BOT_TOKEN")
     initialize_db()
     run_bot(TOKEN)
