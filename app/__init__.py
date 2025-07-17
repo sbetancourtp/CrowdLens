7e8b7dce388f3ba746c038de04a87d5d892925a1
sbetancourtp <sbetancourtp@unal.edu.co>
2025-07-17 08:27:49 -0500

    chore: Small module code sorting.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from app.routes.dashboard_routes import dashboard_bp
 
 
 def create_app():
     app = Flask(__name__)
-
-    from app.routes.dashboard_routes import dashboard_bp
     app.register_blueprint(dashboard_bp)
-
     return app
